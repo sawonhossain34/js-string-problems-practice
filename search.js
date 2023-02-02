5d7409d620dba2d96e5ad11ea963caa3b8b43e0b
sawonhossain34 <sawonhossain3491@gmail.com>
2023-02-02 16:32:50 +0600

    first commit

---
+++ search.js
@@ -0,0 +1,29 @@
+// includes search 
+
+let gan = 'i am a student in from bangladesh souTh Asia';
+// let lyric = 'souTh';
+// let searchOneLine = gan.toLowerCase().includes(lyric.toLowerCase());
+// console.log(searchOneLine);
+
+let bioData = 'i am a student in from bangladesh souTh Asia';
+if(bioData.indexOf('froM') !== -1){
+    console.log('exites inside the string')
+}else{
+    console.log('cannot find it');
+}
+
+
+
+
+
+
+// console.log(fristName , lastName);
+
+// const userName = 'sawOnhossaIn';
+// const userOutput = ' sawonHossain';
+
+// if(userName.toLowerCase == userOutput.toLowerCase){
+//     console.log('valid user');
+// }
+// else{console.log('invalid user');
+// }
---
+++ split-join.js
@@ -0,0 +1,20 @@
+// split apply
+
+// const bioData = 'i am a student in from . Bangladesh souTh Asia';
+// const letter = bioData.split('');
+// const words = bioData.split(' ');
+// const sentence = bioData.split('.')
+// console.log(sentence);
+
+
+// slice apply  
+// const bioData = 'i am a student in from . Bangladesh souTh Asia';
+// const cut = bioData.slice(3, 11);
+// console.log(cut);
+
+
+
+// join apply
+const lylices =['tumi bondhu kala pakhi ami jano', 'ki bosonto kale ami bolte pari sada kala rong',' jomece sada kala bosonto kale ']
+const joi = lylices.join('?');
+console.log(joi);
