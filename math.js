b4fc86fbf1138da0709ca5a219008b47a00e2c5e
sawonhossain34 <sawonhossain3491@gmail.com>
2023-02-02 17:15:58 +0600

    math random round floor ceil file add

---
+++ math.js
@@ -0,0 +1,35 @@
+// pow math 
+
+// const result = Math.pow(3 , 8)
+// console.log(result);
+
+
+
+// math abs 
+// const num1 = 25;
+// const num2 = 45;
+// const result = Math.abs( num1 - num2);
+// if(result < 5){
+//     console.log('dure dure thako')
+// }
+// else{
+//     console.log('friend hoba');
+// }
+
+
+// math round 
+// const number = 5.536 ;
+// const result =Math.round(number);
+// console.log(result);
+
+
+// math celi  
+
+
+// const number = 5.2365;
+// const result = Math.ceil(number);
+// const result1 =Math.floor(number);
+// console.log(result1);
+
+const result = Math.random()*100;
+console.log(result);
